9fe2964e6ae99765ff61fc08ca9686d9a29fcc22
Albertas <albertas.rinkevicius@gmail.com>
2022-05-06 18:05:49 +0300

    Multiple useReducers

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 
 import CounterOne from "./components/CounterOne";
 import CounterTwo from "./components/CounterTwo";
+import CounterThree from "./components/CounterThree";
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne /> */}
-      <CounterTwo />
+      {/* <CounterTwo /> */}
+      
+      {/* Multiple useReducers */}
+      <CounterThree />
     </div>
   );
 }
